8dc7899ba2983cf82561cb8980b7ece1f692051f
Chun Shen <chunshen@wayne.edu>
2026-02-18 10:59:13 -0500

    init commit
    import codes from hydro_analysis code package and simplify the read in
    methods.

---
+++ src/SurfaceFinder.h
@@ -0,0 +1,31 @@
+// Copyright Chun Shen @ 2015
+#ifndef SRC_SurfaceFinder_H_
+#define SRC_SurfaceFinder_H_
+
+#include<memory>
+
+#include "Hydroinfo_MUSIC.h"
+#include "ParameterReader.h"
+
+class SurfaceFinder {
+ private:
+    int hydro_type;
+    std::shared_ptr<Hydroinfo_MUSIC> hydroinfo_MUSIC_ptr;
+    ParameterReader *paraRdr;
+    double e_sw_;
+
+ public:
+    SurfaceFinder() = delete;
+
+    SurfaceFinder(std::shared_ptr<Hydroinfo_MUSIC> hydroinfo_ptr_in,
+                  ParameterReader* paraRdr_in);
+    SurfaceFinder(std::shared_ptr<Hydroinfo_MUSIC> hydroinfo_ptr_in,
+                  ParameterReader* paraRdr_in, double e_sw);
+    ~SurfaceFinder();
+
+    bool check_intersect(double e_sw, double tau, double x, double y,
+                         double dt, double dx, double dy, double ***cube);
+    int Find_full_hypersurface();
+};
+
+#endif  // SRC_SurfaceFinder_H_
---
+++ src/main.cpp
@@ -0,0 +1,72 @@
+/////////////////////////////////////////////////////////////////////////
+//                      MLhydroAfterburn
+//
+//              author: Chun Shen <chunshen@wayne.edu>
+//              Copyright: Chun Shen 2026
+//
+//  This program load hydrodynamic evolution files and find hyper-surface
+//  at a given energy density
+//
+//
+/////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <iomanip>
+#include <memory>
+
+#include "Hydroinfo_MUSIC.h"
+#include "Stopwatch.h"
+#include "ParameterReader.h"
+#include "SurfaceFinder.h"
+
+
+int main(int argc, char *argv[]) {
+    ParameterReader *paraRdr = new ParameterReader();
+    paraRdr->readFromFile("parameters.input");
+    paraRdr->readFromArguments(argc, argv);
+    paraRdr->echo();
+
+    int load_viscous = paraRdr->getVal("load_viscous_info");
+    int hydro_type   = paraRdr->getVal("hydro_type");
+
+    std::shared_ptr<Hydroinfo_MUSIC> hydroinfo_ptr = (
+            std::make_shared<Hydroinfo_MUSIC>());
+
+    Stopwatch sw;
+    sw.tic();
+    // hydro data file pointer
+    if (hydro_type == 3) {
+        int hydro_mode = 9;
+        hydroinfo_ptr->readHydroData(hydro_mode);
+    } else if (hydro_type == 31) {
+        int hydro_mode = 11;
+        hydroinfo_ptr->readHydroData(hydro_mode);
+    } else if (hydro_type == 4) {
+        int hydro_mode = 10;
+        hydroinfo_ptr->readHydroData(hydro_mode);
+    } else {
+        cout << "main: unrecognized hydro_type = " << hydro_type << endl;
+        exit(1);
+    }
+
+    //FluidcellStatistic fluidcellanalysis(hydroinfo_ptr_in, paraRdr);
+    //fluidcellanalysis.outputTempasTauvsX();
+    //fluidcellanalysis.outputKnudersonNumberasTauvsX();
+    //fluidcellanalysis.outputinverseReynoldsNumberasTauvsX();
+    ////double T_cut = paraRdr->getVal("T_cut");
+    ////fluidcellanalysis.analysis_hydro_volume_for_photon(T_cut);
+    //fluidcellanalysis.output_temperature_vs_avg_utau();
+    //fluidcellanalysis.output_flowvelocity_vs_tau();
+
+    // construct freeze-out hyper-surface
+    // SurfaceFinder* surface_ptr = new SurfaceFinder(hydroinfo_ptr, paraRdr);
+    // surface_ptr->Find_full_hypersurface();
+
+    sw.toc();
+    std::cout << "totally takes : " << sw.takeTime() << " seconds."
+              << std::endl;
+
+    return(0);
+}
+
+
